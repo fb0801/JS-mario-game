253d9701b8a3aee1420787ebafcfeb12d3bd6482
fb0801 <fbhatti0801@gmail.com>
2021-10-19 10:22:43 +0100

    adding features

--- game.js
+++ game.js
@@ -3,11 +3,16 @@ kaboom({
     fullscreen: true,
     scale: 1,
     debug: true,
+    clearColor: [0,0,0,1],
 })
 
 scene("game", () =>{
+    layers(['bg','obj','ui'],'obj')
 
-
+    loadRoot('')
+    loadSprite('')
+    loadSprite('')
+    loadSprite('')
 })
 
 start("game")
